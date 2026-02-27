57d3e46a3ebfb6cd3390da249e85ce7fba44164f
Gjorgdy <jordy.reins@proton.me>
2026-02-27 17:33:35 +0100

    1.21.11 ADD: initial

---
+++ src/main/java/nl/gjorgdy/golem_disk_jockey/GolemDiscJockey.java
@@ -0,0 +1,10 @@
+package nl.gjorgdy.golem_disk_jockey;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GolemDiscJockey implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
